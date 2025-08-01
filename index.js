e76a0fab213bc4715cf07bb0a97100de7de42cec
mustafiz8g <mustafizur8g@gmail.com>
2025-08-02 02:23:19 +0600

    dfjdskf

--- index.js
+++ index.js
@@ -20,17 +20,43 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+let mealCollection;
+
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+     const db = client.db('mealTracker');
+         mealCollection = db.collection('meals');
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+// POST meal
+app.post('/api/meals', async (req, res) => {
+  const meal = req.body;
+  const result = await mealCollection.insertOne(meal);
+  res.send(result);
+});
+
+// GET meals for one user
+app.get('/api/meals/my', async (req, res) => {
+  const { uid } = req.query;
+  const meals = await mealCollection.find({ uid }).toArray();
+  res.send(meals);
+});
+
+// GET all meals
+app.get('/api/meals/all', async (req, res) => {
+  const meals = await mealCollection.find().toArray();
+  res.send(meals);
+});
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -38,6 +64,6 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello from RealScape Server..')
+    res.send('Hello from Mess_Meal Server..')
 })
 app.listen(port, () => console.log(`Server running on port ${port}`));
